feccca40bcd91eaf2a6c8a565c228df19e4cb345
vikaslawand <132754314+vikaslawand@users.noreply.github.com>
2024-07-23 19:24:21 +0530

    Add files via upload

---
+++ bookstore/header.php
@@ -0,0 +1,19 @@
+<header class="header">
+
+   <div class="flex">
+
+      <a href="#" class="logo">Book_S</a>
+
+      <nav class="navbar">
+         <!-- <a href="admin.php">add products</a>
+         <a href="products.php">view products</a> -->
+      </nav>
+
+
+      <!-- <a href="cart.php" class="cart">cart <span>0</span> </a> -->
+
+      
+
+   </div>
+
+</header>
